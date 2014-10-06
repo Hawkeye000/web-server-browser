59eef9356ba877eb02332c1089f15d5ae60c34d1
Benjamin Pearce <hawk.git@bcpearce.com>
2014-10-05 21:51:42 -0400

    edited response and headers to serve the file content

--- lib/simple_server.rb
+++ lib/simple_server.rb
@@ -1,4 +1,5 @@
 require 'socket'
+require 'ptools'
 
 HTTP_METHODS = ["GET", "HEAD", "POST", "PUT", "DELETE", "TRACE", "CONNECT"]
 
@@ -14,27 +15,45 @@ def identify_resource(initial_request_line)
   end
 end
 
+def bin_or_text(file_path)
+  "bin" if File.binary?(file_path)
+  "text" if File.binary?(file_path)
+end
+
 port = 2000
 
 server = TCPServer.open(port)
 loop { 
   client = server.accept
   lines = []
+  headers = []
 
   while line = client.gets and line !~ /^\s*$/
     lines << line.chomp
   end
 
-  puts "Received: #{lines.join("<br \>")}"
+  puts "Received: #{lines.join("\n")}"
+
+  puts identify_resource(lines[0])
 
   if identify_http_method(lines[0]) == "GET"
     file_path = identify_resource(lines[0])
-    response = file_path
-    headers = ["http/1.0 200 OK",\
-        "date: #{Time.now.ctime}",\
-        "server: localhost",\
-        "content-type: text/html; charset=iso-8859-1",\
-        "content-length: #{response.length}\r\n\r\n"].join("\r\n")
+    file_path[0] = "" # remove the leading root indicator
+    if File.exists?(file_path)
+      puts "Serving: #{file_path}"
+      headers << "http/1.0 200 OK"
+      headers << "server: localhost"
+      headers << "content-type: #{bin_or_text(file_path)}/#{File.extname(file_path).delete('.')}"
+      headers << "content-length: #{File.size(file_path)}\r\n\r\n"
+
+      response = File.read(file_path)
+    else
+      headers << "http/1.0 404 Not Found" unless File.exists?(file_path)
+
+      response = "404 Not Found"
+    end
+  else
+    response = File.read("404_not_found.html")
   end
 
   client.puts headers
