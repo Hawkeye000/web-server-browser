9f835123cf07cb956df5d4a6f1b0578023b1dfb3
Benjamin Pearce <hawk.git@bcpearce.com>
2014-10-06 21:21:20 -0400

    refactored simple_tiny_browser.rb to loop and accept continuous requests

--- lib/simple_tiny_browser.rb
+++ lib/simple_tiny_browser.rb
@@ -8,15 +8,21 @@ path = '/index.html'
 puts "Enter HTTP Method:"
 request_method = gets.chomp
 
-if HTTP_METHODS.any? { |method| method == request_method }
+loop {
+  if HTTP_METHODS.any? { |method| method == request_method }
 
-  request = "#{request_method} #{path} HTTP/1.0\r\n\r\n"
+    puts "Enter a URL:"
+    host, root, path = gets.chomp.partition('/')
+    path.prepend(root)
 
-end
+    request = "#{request_method} #{path} HTTP/1.0\r\n\r\n"
 
-socket = TCPSocket.open(host, port)
-socket.print(request)
-response = socket.read
-# split response at first blank line into headers and body
-headers, body = response.split("\r\n\r\n", 2)
-print body
+    socket = TCPSocket.open(host, port)
+    socket.print(request)
+    response = socket.read
+    # split response at first blank line into headers and body
+    headers, body = response.split("\r\n\r\n", 2)
+    print body
+
+  end 
+}
